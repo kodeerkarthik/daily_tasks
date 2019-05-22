1405984053d06346c42c337a7cac2c10d5a07f34
karthik <kkrishnegowdaostb1@gmail.com>
2019-05-22 15:50:54 +0530

    changes done in task3

--- jstask3/javascript/index.js
+++ jstask3/javascript/index.js
@@ -1,31 +1,18 @@
-var year = 2016;
-var month = 5;
+var year = 2018;
+var month = 2;
 if(year%4==0){	
 	document.write(year+" is leap year <br>");
 }
 else{
-	month=13;
+	var temp=1;
 	document.write(year+" is not leap year <br>");
 }
 switch(month){
-	case 1:document.write("31 days");
+	case 1:case 3:case 5:case 7:case 8: case 10:case 12: document.write("31 days");
 	break;
-	case 3:document.write("31 days");
+	case 2:if(temp==1)document.write("28 days");else document.write("29 days");
 	break;
-	case 5:document.write("31 days");
-	break;
-	case 7:document.write("31 days");
-	break;
-	case 8: document.write("31 days");
-	break;
-	case 10: document.write("31 days");
-	break;
-	case 12: document.write("31 days");
-	break;
-	case 2:document.write("29 days");
-	break;
-	case 13:document.write("28 days");
-	break;
-	default:document.write("30 days");
+	case 4:case 6: case 9: case 11:document.write("30 days");
+	default:document.write("please give month less than 13 value");
 	break;
 }
