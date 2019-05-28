7eff6158c132d0d251068d28ee8dd4f9a3cd3116
karthik <kkrishnegowdaostb1@gmail.com>
2019-05-28 16:13:23 +0530

    jstask6 completed

---
+++ jstask6/javascript/index.js
@@ -0,0 +1,26 @@
+function selectAll(){
+	var items=document.getElementsByName('check');
+	for(var i=0; i<items.length; i++){
+		if(items[i].type=='checkbox')
+			items[i].checked=true;
+	}
+}
+			
+function UnSelectAll(){
+	var items=document.getElementsByName('check');
+	for(var i=0; i<items.length; i++){
+		if(items[i].type=='checkbox')
+			items[i].checked=false;
+	}
+}	
+function calculate(){
+	var total=0;
+	var items=document.getElementsByName('check');
+	for(var i=0; i<items.length; i++){
+		if(items[i].type=='checkbox' && items[i].checked==true)
+				total=parseInt(items[i].value)+total;
+	}
+	// document.write("the total is :"+total);
+	document.getElementById("output").innerHTML="the total is : "+total;	
+}
+ 
